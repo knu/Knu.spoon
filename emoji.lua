147ca5f97032c9fc4b581ed6853e28a6d5ee991e
Akinori MUSHA <knu@idaemons.org>
2019-02-25 20:50:12 +0900

    Migrate from gemoji to emoji-db for latest emojis

--- emoji.lua
+++ emoji.lua
@@ -1,16 +1,40 @@
 local emoji = {}
 
+local unescape = function (text)
+  -- To decode "Keycap: \\x{23}"
+  return text:gsub(
+    "\\x{(%x+)}",
+    function (h)
+      return utf8.char(tonumber(h, 16))
+    end
+  )
+end
+
+local capitalize = function (c, r)
+  return string.upper(c) .. r
+end
+
+local titlecase = function (text)
+  return text:gsub("^(%l)(%w*)", capitalize):gsub("( %l)(%w*)", capitalize)
+end
+
+local underscore = function (text)
+  return text:gsub("[%s%-]+", "_"):gsub("([^%w_])", function (c)
+    if c == 'ō' then
+      -- We just type gyoza for gyōza.
+      return 'o'
+    else
+      return c
+    end
+  end)
+end
+
 local initializers = {
   table = function ()
-    local f = io.open("gemoji/emoji.json", "r")
+    local f = io.open("emoji-db/emoji-db.json", "r")
     if f == nil then
-      hs.execute([[
-      set -e
-      /usr/bin/gem install --user-install gemoji
-      ~/.gem/ruby/2.0.0/bin/gemoji extract gemoji/images
-      /usr/bin/ruby -rgemoji -rfileutils -e 'FileUtils.cp Emoji::data_file, ARGV[0]' gemoji/
-    ]])
-      f = io.open("gemoji/emoji.json", "r")
+      hs.execute("git clone https://github.com/meyer/emoji-db.git")
+      f = io.open("emoji-db/emoji-db.json", "r")
     end
     local json = f:read("a")
     f:close()
@@ -19,41 +43,29 @@ local initializers = {
 
   choices = function ()
     local choices = {}
-    for _, entry in ipairs(emoji.table) do
-      local chars = entry.emoji
-      if chars then
-        local path = ("gemoji/images/unicode/%s.png"):format(
-          chars:gsub(utf8.charpattern, function (char)
-              local cp = utf8.codepoint(char)
-              if cp == 0xfe0f or cp == 0x200d then
-                return ""
-              else
-                return ("%04x-"):format(cp)
-              end
-          end):gsub("-$", "")
-        )
-        local aliases = {}
-        for _, alias in ipairs(entry.aliases) do
-          table.insert(aliases, ":" .. alias .. ":")
+    for key in hs.fnutils.sortByKeys(emoji.table) do
+      local entry = emoji.table[key]
+      local us = hs.fnutils.map(entry.codepoints,
+        function (cp)
+          return ("U+%04X"):format(cp)
         end
-        local us = chars:gsub(utf8.charpattern, function (char)
-            local cp = utf8.codepoint(char)
-            return ("U+%04X "):format(cp)
-        end):gsub(" $", "")
-        table.insert(choices, {
-            text = entry.description:gsub("(%l)(%w*)",
-              function (c, r)
-                return string.upper(c) .. r
-              end
-            ),
-            subText = us .. ": " .. table.concat({
-                table.unpack(aliases),
-                table.unpack(entry.tags)
-            }, ", "),
-            image = hs.image.imageFromPath(path),
-            chars = chars,
-        })
+      )
+      local mnemonics = { entry.emojilib_name }
+      for _, keyword in ipairs(entry.keywords) do
+        local mnemonic = underscore(keyword)
+        if not hs.fnutils.some(mnemonics, function (e) return e == mnemonic end) then
+          table.insert(mnemonics, mnemonic)
+        end
       end
+      local keywords = hs.fnutils.map(mnemonics, function (keyword) return ":" .. keyword .. ":" end)
+      local text = titlecase(unescape(entry.name))
+      local subText = table.concat(us, " ") .. ": " .. table.concat(keywords, ", ")
+      table.insert(choices, {
+        text = text,
+        subText = subText,
+        image = hs.image.imageFromPath("emoji-db/" .. entry.image),
+        chars = entry.emoji,
+      })
     end
 
     return choices
