ec7a02051445e1b838827ccfe3c3158c6569d87e
Akinori MUSHA <knu@idaemons.org>
2022-11-10 22:04:51 +0900

    Implement knu.application.launchInBackground and openInBackground

--- application.lua
+++ application.lua
@@ -1,5 +1,7 @@
 local application = {}
 
+local utils = require((...):gsub("[^.]+$", "utils"))
+
 local registry = {}
 
 local watcher = hs.application.watcher
@@ -72,6 +74,98 @@ application.offChange = function (bundleID, fn)
       return
     end
   end
+end
+
+local launchAppInBackground = function (bundlePathOrID)
+  local bundleID
+  local bundlePath
+  local info = hs.application.infoForBundleID(bundlePathOrID)
+  if info then
+    bundleID = info.CFBundleIdentifier
+  else
+    info = hs.application.infoForBundlePath(bundlePathOrID)
+    if not info then
+      return nil
+    end
+    bundlePath = bundlePathOrID
+    bundleID = info.CFBundleIdentifier
+  end
+
+  local app = hs.application.get(bundleID)
+  if app then
+    return app
+  end
+
+  if bundlePath then
+    hs.execute(utils.shelljoin("/usr/bin/open", "-g", bundlePath))
+  else
+    hs.execute(utils.shelljoin("/usr/bin/open", "-gb", bundleID))
+  end
+
+  return bundleID
+end
+
+-- Launches an application in background if it's not already running and invokes a callback function
+--
+-- Parameters:
+--
+--   - bundlePathOrID - the bundle path or bundle ID of the application to launch
+--   - fn - a callback function which is called with the application object as the only argument, or `nil` if the application could not be found or launched
+--   - wait - (optional) the maximum number of seconds to wait for the app to be launched, if not already running; if omitted, defaults to 0; if the app takes longer than this to launch, this function will return `nil`, but the app will still launch
+--   - waitForFirstWindow - (optional) if `true`, additionally wait until the app has spawned its first window (which usually takes a bit longer)
+application.launchInBackground = function (bundlePathOrID, fn, wait, waitForFirstWindow)
+  local deadline = hs.timer.secondsSinceEpoch() + wait
+  local app = launchAppInBackground(bundlePathOrID)
+
+  if type(app) ~= "string" then
+    fn(app)
+    return
+  end
+
+  local bundleID = app
+  local timer
+  timer = hs.timer.doEvery(
+    1,
+    function ()
+      if hs.timer.secondsSinceEpoch() > deadline then
+        timer:stop()
+        fn(nil)
+      else
+        local app = hs.application.get(bundleID)
+        if app and (not waitForFirstWindow or app:mainWindow()) then
+          timer:stop()
+          fn(app)
+        end
+      end
+    end
+  )
+end
+
+-- Opens an application in background and waits for it to launch if it's not already running, returning the application object
+--
+-- Parameters:
+--
+--   - bundlePathOrID - the bundle path or bundle ID of the application to launch
+--   - wait - (optional) the maximum number of seconds to wait for the app to be launched, if not already running; if omitted, defaults to 0; if the app takes longer than this to launch, this function will return `nil`, but the app will still launch
+--   - waitForFirstWindow - (optional) if `true`, additionally wait until the app has spawned its first window (which usually takes a bit longer)
+application.openInBackground = function (bundlePathOrID, wait, waitForFirstWindow)
+  local deadline = hs.timer.secondsSinceEpoch() + wait
+  local app = launchAppInBackground(bundlePathOrID)
+
+  if type(app) ~= "string" then
+    return app
+  end
+
+  local bundleID = app
+  repeat
+    local app = hs.application.get(bundleID)
+    if app and (not waitForFirstWindow or app:mainWindow()) then
+      return app
+    end
+    hs.timer.usleep(100000)
+  until hs.timer.secondsSinceEpoch() > deadline
+
+  return nil
 end
 
 return application
